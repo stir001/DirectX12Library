c3d818f290a86a345d7e7114bca51b55ded274b5
stir001 <baske.max@icloud.com>
2018-11-21 19:11:34 +0900

    デフォルト引数を設定

--- DirectX12/DrawObject/Primitive3D/PrimitiveCreator.h
+++ DirectX12/DrawObject/Primitive3D/PrimitiveCreator.h
@@ -36,9 +36,9 @@ public:
 
 	void SetLightObject(std::shared_ptr<LightObject> inlight);
 	std::shared_ptr<PrimitiveController> CreatePlane(DirectX::XMFLOAT3 pos, float length, float height, DirectX::XMFLOAT3 normal);
-	std::shared_ptr<PrimitiveController> CreateCube(float length, const std::string& texPath);
+	std::shared_ptr<PrimitiveController> CreateCube(float length, const std::string& texPath = "");
 	std::shared_ptr<PrimitiveController> CreateCubeNormalMap(float length, const std::string& texPath);
-	std::shared_ptr<PrimitiveController> CreateSphere(float radius, unsigned int div, const std::string& texPath);
+	std::shared_ptr<PrimitiveController> CreateSphere(float radius, unsigned int div, const std::string& texPath = "");
 	void SetParamaters(std::shared_ptr<PrimitiveController>& ctrl);
 private:
 	PrimitiveCreator();
